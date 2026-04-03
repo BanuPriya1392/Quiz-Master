9f725c90ce1c9ca181502c62886785494822e92c
priyaraja32 <rpriyanka31032004@gmail.com>
2026-04-03 08:08:55 +0530

    wishlist api added

---
+++ src/models/Wishlist.js
@@ -0,0 +1,17 @@
+// models/Wishlist.js
+import mongoose from "mongoose";
+
+const wishlistSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  question: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Question",
+    required: true,
+  },
+}, { timestamps: true });
+
+export default mongoose.model("Wishlist", wishlistSchema);
