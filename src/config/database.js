c8fa2e9c4c86035a8fef3c23c5aff14a380507c0
BanuPriya1392 <banusangee@gmail.com>
2026-04-09 17:39:25 +0530

    Updated Code

--- src/config/database.js
+++ src/config/database.js
@@ -3,15 +3,12 @@ import mongoose from "mongoose";
 // Function to connect MongoDB
 const connectDB = async () => {
   try {
-
     // connect to MongoDB using connection string from .env
-    await mongoose.connect("mongodb://localhost:27017/Quiz-Master");
+    await mongoose.connect(process.env.MONGO_URI);
 
     // success message
     console.log("MongoDB Connected Successfully");
-
   } catch (error) {
-
     // error message if connection fails
     console.log("Database connection error:", error);
 
